251e077b4004ff963a3b1f5de0a2623b8607cf5e
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-07-26 17:14:26 +0500

    job schema added

--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -5,3 +5,20 @@ export interface IPost extends Document {
     content: string;
     user: mongoose.Types.ObjectId;
 }
+export // Berilgan fayldagi ma'lumotlar uchun schema interfeysi
+    interface IFileData extends Document {
+    comImg: string;
+    comName: string;
+    comLocation: string;
+    jobSave: boolean;
+    jobTitle: string;
+    jobInfo: string;
+    jobSkills: Types.ObjectId[]; // ObjectId lar uchun ref
+    jobType: string;
+    jobCooperate: boolean;
+    jobPrice: number;
+    jobEmployee: Types.ObjectId[]; // ObjectId uchun ref
+    moreInfo: Types.ObjectId[]; // ObjectId uchun ref
+    catId: string;
+    moneyTypeId: Types.ObjectId; // ObjectId uchun ref
+}
---
+++ src/schemas/job.schema.ts
@@ -0,0 +1,74 @@
+import { Schema, model, Document, Types } from 'mongoose';
+import validator from 'validator';
+import { IFileData } from '../interface/interface';
+
+const FileDataSchema = new Schema<IFileData>({
+    comImg: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (value: string) => validator.isURL(value),
+            message: 'Noto\'g\'ri URL formati',
+        },
+    },
+    comName: {
+        type: String,
+        required: true,
+    },
+    comLocation: {
+        type: String,
+        required: true,
+    },
+    jobSave: {
+        type: Boolean,
+        required: true,
+    },
+    jobTitle: {
+        type: String,
+        required: true,
+    },
+    jobInfo: {
+        type: String,
+        required: true,
+    },
+    jobSkills: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Skill', 
+        },
+    ],
+    jobType: {
+        type: String,
+        required: true,
+    },
+    jobCooperate: {
+        type: Boolean,
+        required: true,
+    },
+    jobPrice: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    jobEmployee: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Employee', 
+    }],
+    moreInfo:[ {
+        type: Schema.Types.ObjectId,
+        ref: 'MoreInfo', 
+    }],
+    catId: {
+        type: String,
+        required: true,
+    },
+    moneyTypeId: {
+        type: Schema.Types.ObjectId,
+        ref: 'TypeOfMoney', // "TypeOfMoney" nomli boshqa modelga bog'lanish uchun ref
+    },
+});
+
+// Model yaratish
+const FileDataModel = model<IFileData>('FileData', FileDataSchema);
+
+export default FileDataModel;
