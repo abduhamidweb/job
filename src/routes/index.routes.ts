89bfceab7e712b74ac2258cef7fa9721ee92bd3d
shahboz <shahbozmamatkarimov2303@gmail.com>
2023-07-27 15:09:22 +0300

    OK

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -4,10 +4,8 @@ import authMiddleware from "../middleware/auth.js";
 import jobCategoryRouter from "./jobCategory.routes.js"
 const router = express.Router();
 
-
 router.use('/test', ()=>{});
 router.use('/job', jobRouter);
 router.use('/category', jobCategoryRouter);
 
 export default router
-
