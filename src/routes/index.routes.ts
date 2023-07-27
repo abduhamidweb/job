27d96595d36eaa5932d9b34319b0d3c451593e8b
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-07-27 14:39:14 +0500

    dev branch has finished

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import jobRouter from "./job.routes.js"
 import authMiddleware from "../middleware/auth.js";
-
+import jobCategoryRouter from "./jobCategory.routes.js"
 const router = express.Router();
 
 
 router.use('/test', ()=>{});
-
+router.use('/job', jobRouter);
+router.use('/category', jobCategoryRouter);
 
 export default router
+
