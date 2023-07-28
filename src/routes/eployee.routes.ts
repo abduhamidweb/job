08825b0a4cc9239343ffced66fa747cb556bb747
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-07-28 16:56:48 +0500

    all branch finshed

--- src/routes/eployee.routes.ts
+++ src/routes/eployee.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import EmployeeController from '../controllers/employee.controller.js';
+import authMiddleware from '../middleware/auth.js';
 
 export const router = Router();
-router.post('/', EmployeeController.postEmployee.bind(EmployeeController));
+router.post('/', authMiddleware,EmployeeController.postEmployee.bind(EmployeeController));
 router.get('/', EmployeeController.getAllEmployee.bind(EmployeeController));
