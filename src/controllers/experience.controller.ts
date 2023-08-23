8a5973a80781ab7aeb0bc1660985e22fbd2c5c20
ZiyodillaDev <ziyodillazokirkhujayev@gmail.com>
2023-08-23 20:16:18 +0500

    experience api updated

--- src/controllers/experience.controller.ts
+++ src/controllers/experience.controller.ts
@@ -6,15 +6,24 @@ import { JWT } from "../utils/jwt.js";
 import userSchema from "../schemas/user.schema.js";
 
 class ExperienceController {
-    public async postExperience(req: Request, res: Response): Promise<void> {
-      
-          
-        try {
-        const token = req.headers.token as string;
-        let userId = JWT.VERIFY(token as string).id;
-        const { experience, remoteExperience }: IExperience = req.body;
+
+  public async getAllExperience(req: Request, res: Response): Promise<void> {
+    try {
+      const experiences: IExperience[] | null = await Experiences.find();
+
+      res.status(200).json(experiences);
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  public async updateExperience(req: Request, res: Response): Promise<void> {
+    try {
+      const token = req.headers.token as string;
+      let userId = JWT.VERIFY(token as string).id;
+      const { experience, remoteExperience }: IExperience = req.body;
       if (!userId) {
-        const errorMessage = "userId kiritilmagan";
+        const errorMessage = "Bunday user IDlik malumot topilmadi";
         res.status(400).json({ message: errorMessage, status: 400 });
         return;
       }
@@ -28,68 +37,37 @@ class ExperienceController {
         return;
       }
 
-      const experiencePost = new Experiences({
-        experience,
-        remoteExperience,
-        userId,
-      });
+      console.log(userId)
 
-      await experiencePost.save();
-      await userSchema.findByIdAndUpdate(userId, {
-        experience: experiencePost._id,
-      });
-      res.status(201).json("Successfully created Experience");
-    } catch (error: any) {
-      res.status(500).json({ error: error.message });
-    }
-  } 
-
-  public async getAllExperience(req: Request, res: Response): Promise<void> {
-    try {
-      const experiences: IExperience[] | null = await Experiences.find();
-
-      res.status(200).json(experiences);
-    } catch (error: any) {
-      res.status(500).json({ error: error.message });
-    }
-  }
-
-  public async updateExperience(req: Request, res: Response): Promise<void> {
-    const { id } = req.params;
-    const { experience, remoteExperience }: IExperience = req.body;
-
-    try {
-      const experiencePut = await Experiences.findByIdAndUpdate(
-        id,
+      const experiencePut = await Experiences.findOneAndUpdate(
+        {userId},
         { experience, remoteExperience },
         { new: true }
       );
+      console.log(experiencePut);
+
 
       if (!experiencePut) {
-        res.status(404).json({ error: "Experience not found" });
+        const experiencePost = new Experiences({
+          experience,
+          remoteExperience,
+          userId,
+        });
+
+        await experiencePost.save();
+        await userSchema.findByIdAndUpdate(userId, {
+          experience: experiencePost._id,
+        });
+        res.status(201).json("Successfully created Experience");
       }
-      res.status(200).send("Skill succesfully edited");
+      else {
+        res.status(200).json("Experience succesfully edited");
+      }
     } catch (error: any) {
       res.status(500).json({ error: error.message });
     }
   }
 
-  public async deleteExperience(req: Request, res: Response): Promise<void> {
-    const { id } = req.params;
-
-    try {
-      const experience: IExperience | null =
-        await Experiences.findByIdAndDelete(id);
-
-      if (!experience) {
-        res.status(404).json({ error: "Experience not found" });
-      }
-
-      res.status(200).send("Experience succesfully deleted");
-    } catch (error: any) {
-      res.status(500).json({ error: error.message });
-    }
-  }
 }
 
 export default new ExperienceController();
--- src/routes/experience.routes.ts
+++ src/routes/experience.routes.ts
@@ -3,7 +3,5 @@ import ExperienceController from '../controllers/experience.controller.js';
 
 export const router = Router();
 
-router.post('/', ExperienceController.postExperience.bind(ExperienceController));
 router.get('/', ExperienceController.getAllExperience.bind(ExperienceController));
-router.put('/:id', ExperienceController.updateExperience.bind(ExperienceController));
-router.delete('/:id', ExperienceController.deleteExperience.bind(ExperienceController));
+router.put('/', ExperienceController.updateExperience.bind(ExperienceController));
